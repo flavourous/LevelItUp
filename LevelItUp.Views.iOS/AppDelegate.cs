1e9028f3ecdb010f171a710afb6b80e2e13db0a7
David Higgins <d_higgins@imap.cc>
2018-02-23 21:59:14 +0000

    ok that is better

--- LevelItUp.Views.iOS/AppDelegate.cs
+++ LevelItUp.Views.iOS/AppDelegate.cs
@@ -3,10 +3,12 @@ using Foundation;
 using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Push;
 using MvvmCross.Core.ViewModels;
 using MvvmCross.Forms.iOS;
 using MvvmCross.Platform;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using UIKit;
 
@@ -19,7 +21,10 @@ namespace LevelItUp.Views.iOS
 
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            AppCenter.Start("f1388e2a-caa8-4130-b09a-94ad36ea0e87", typeof(Analytics), typeof(Crashes));
+            SemaphoreSlim s = new SemaphoreSlim(0, 1);
+            Crashes.FailedToSendErrorReport += (o, e) => s.Release();
+            Crashes.SentErrorReport += (o, e) => s.Release();
+            AppCenter.Start("f1388e2a-caa8-4130-b09a-94ad36ea0e87", typeof(Analytics), typeof(Crashes), typeof(Push));
             AppDomain.CurrentDomain.UnhandledException += (o, e) => Console.WriteLine("Exception Raised{0}----------------{0}{1}", Environment.NewLine, e.ExceptionObject);
 
             Window = new UIWindow(UIScreen.MainScreen.Bounds);
@@ -31,7 +36,7 @@ namespace LevelItUp.Views.iOS
                 Window.MakeKeyAndVisible();
                 Task.Run(async () =>
                 {
-                    await Task.Delay(10000);
+                    await s.WaitAsync(10000);
                     BeginInvokeOnMainThread(StartMvvMxForms);
                 });
             }
@@ -60,16 +65,17 @@ namespace LevelItUp.Views.iOS
         {
             base.ViewDidLoad();
 
-            Title = "...Loading...";
-            View.BackgroundColor = UIColor.Cyan;
+            Title = "Starting Up";
+            View.BackgroundColor = UIColor.Purple;
 
             // keep the code the username UITextField
             load = new UILabel
             {
-                BackgroundColor = UIColor.White,
-                TextColor = UIColor.Black,
-                Text = "Uploading crash data :("
+                TextAlignment = UITextAlignment.Center,
+                TextColor = UIColor.White,
+                Text = "Uploading crash data\n(._.)"
             };
+            load.Font = UIFont.BoldSystemFontOfSize(24f);
 
             View.AddSubview(load);
         }
