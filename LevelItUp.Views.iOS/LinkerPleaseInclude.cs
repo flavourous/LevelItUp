004efbe61dd9ea1502b4a9830f64ccd553db4ecf
David Higgins <d_higgins@imap.cc>
2018-02-24 00:27:32 +0000

    added consolecolor to linkerinclude

---
+++ LevelItUp.Views.iOS/LinkerPleaseInclude.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace LevelItUp.Views.iOS
+{
+    public class LinkerPleaseInclude
+    {
+        public void Include(ConsoleColor color)
+        {
+            Console.Write("");
+            Console.WriteLine("");
+            color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+        }
+    }
+}
